13be504ceaed3cc045b249027cf55447d10cda48
Danuel <public.danuel@gmail.com>
2020-12-14 14:46:27 +0900

    Fix memory leak by selecting files

--- src/utils.js
+++ src/utils.js
@@ -119,55 +119,76 @@ module.exports = class Utils {
   static selectFiles(config = {}) {
     return new Promise((resolve, reject) => {
       /**
-       * Create a new INPUT element
-       * @type {HTMLElement}
+       * Check
        */
-      let inputElement = document.createElement('INPUT');
+      if (input === null) {
+        /**
+         * Create a new INPUT element
+         * @type {HTMLElement}
+         */
+        let inputElement = document.createElement('INPUT');
 
-      /**
-       * Set a 'FILE' type for this input element
-       * @type {string}
-       */
-      inputElement.type = 'file';
+        /**
+         * Set a 'FILE' type for this input element
+         * @type {string}
+         */
+        inputElement.type = 'file';
 
-      if (config.multiple) {
-        inputElement.setAttribute('multiple', 'multiple');
+        if (config.multiple) {
+          inputElement.setAttribute('multiple', 'multiple');
+        }
+
+        if (config.accept) {
+          inputElement.setAttribute('accept', config.accept);
+        }
+
+        /**
+         * Do not show element
+         */
+        inputElement.style.display = 'none';
+
+        /**
+         * Append element to the body
+         * Fix using module on mobile devices
+         */
+        document.body.appendChild(inputElement);
+
+        /**
+         * Create event handler for resolving files
+         * @param {Event} event
+         */
+        const resolveFiles = event => {
+          /**
+           * Get files from input field
+           */
+          const files = event.target.files;
+
+          /**
+           * Return ready to be uploaded files array
+           */
+          resolve(files);
+
+          /**
+           * Remove event handler for preventing memory leak
+           */
+          inputElement.removeEventListener(resolveFiles);
+
+          /**
+           * Remove element from a DOM
+           */
+          document.body.removeChild(inputElement);
+
+          /**
+           * Remove input element
+           */
+          inputElement = null;
+        };
+
+        /**
+         * Add onchange listener for «choose file» pop-up
+         */
+        inputElement.addEventListener('change', resolveFiles, false);
       }
-
-      if (config.accept) {
-        inputElement.setAttribute('accept', config.accept);
-      }
-
-      /**
-       * Do not show element
-       */
-      inputElement.style.display = 'none';
-
-      /**
-       * Append element to the body
-       * Fix using module on mobile devices
-       */
-      document.body.appendChild(inputElement);
-
-      /**
-       * Add onchange listener for «choose file» pop-up
-       */
-      inputElement.addEventListener('change', event => {
-        /**
-         * Get files from input field
-         */
-        const files = event.target.files;
-
-        /**
-         * Return ready to be uploaded files array
-         */
-        resolve(files);
-
-        /**
-         * Remove element from a DOM
-         */
-        document.body.removeChild(inputElement);
-      }, false);
 
       /**
        * Fire click event on «input file» field
@@ -233,3 +254,9 @@ module.exports = class Utils {
     return headerMap;
   }
 };
+
+/**
+ * Cached input element for preventing memory leak
+ * @type {HTMLInputElement}
+ */
+let inputElement = null;
